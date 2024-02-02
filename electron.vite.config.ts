89773d50b176faeed1b80eedcc1a655fe6fdcf90
Ceasar2001 <ecleoceasar1@gmail.com>
2024-02-02 20:43:15 +0800

    configuring

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -7,12 +7,25 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
+    plugins: [externalizeDepsPlugin()],
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@/lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared')
+      }
+    }
+  },
+  renderer: {
+    assetsInclude: 'src/renderer/assets/**',
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'),
+        '@/hooks': resolve('src/renderer/src/hooks'),
+        '@/assets': resolve('src/renderer/src/assets'),
+        '@/store': resolve('src/renderer/src/store'),
+        '@/components': resolve('src/renderer/src/components'),
+        '@/mocks': resolve('src/renderer/src/mocks'),
       }
     },
     plugins: [react()]
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,9 @@
-function App(): JSX.Element {
-  return <div></div>
+function App() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl text-red-500">Hello World</span>
+    </div>
+  )
 }
 
 export default App
